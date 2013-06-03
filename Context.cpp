3b1d0c7680776d206ecd151876053097c0835edf
dereklmc <derek.l.mclean@gmail.com>
2013-06-02 18:54:47 -0700

    Documented context class

--- Context.cpp
+++ Context.cpp
@@ -1,5 +1,25 @@
+/**
+ * @file Context.cpp
+ * @brief Definition of functions in Context.h
+ *
+ * Currently, defines a constructor for Context to ease initialization of a
+ * context object.
+ *
+ * @author Mitch Carlson, Derek McLean
+ * @version 1.0
+ */
+
 #include "Context.h"
 
+/**
+ * Constructor.
+ *
+ * Creates context from external state variables.
+ *
+ * @param input - reference to external input stream (e.g. std::cin)
+ * @param output - reference to external ouput stream (e.g. std::cout)
+ * @param FTPClient - represents a network connection to an ftp server
+ */
 Context::Context(std::istream &input, std::ostream &output, FTPClient *ftp) :
     input(&input),
     output(&output),
--- Context.h
+++ Context.h
@@ -1,3 +1,24 @@
+/**
+ * @file Context.h
+ * @brief Representation of external state that a command is applied to.
+ *
+ * The external state includes all interfaces to external dependencies of the
+ * program, such as:
+ *
+ *  - I/O streams
+ *  - Network Connections
+ *  - Configuration
+ *
+ * Currently, context manages two different concerns: I/O and FTP connections.
+ * While these should be in separate contexts, sacrificing flexibility allows
+ * the current design to simply meet the requirements. Future iterations should
+ * implement multiple context handling.
+ *
+ * @author Mitch Carlson, Derek McLean
+ * @version 1.0
+ */
+
+
 #ifndef __CONTEXT__
 #define __CONTEXT__
 
@@ -5,13 +26,24 @@
 
 #include "FTPClient.h"
 
+/** External context of commands */
 class Context {
     public:
+
+        /**
+         * Constructor.
+         *
+         * Creates context from external state variables.
+         *
+         * @param input - reference to external input stream (e.g. std::cin)
+         * @param output - reference to external ouput stream (e.g. std::cout)
+         * @param FTPClient - represents a network connection to an ftp server
+         */
         Context(std::istream &input, std::ostream &output, FTPClient *ftp=NULL);
 
-        std::istream *input;
-        std::ostream *output;
-        FTPClient *ftp;
+        std::istream *input;  //!< ptr to input stream of external context
+        std::ostream *output;  //!< ptr to output stream of external context
+        FTPClient *ftp;  //!< ptr to ftp network connection in context
 };
 
 #endif
